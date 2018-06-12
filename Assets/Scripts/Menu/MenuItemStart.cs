dcd673a5c071b49eb2582e93478469323d79710b
valerycpp.pc <valerycpp.pc@home.com>
2018-06-13 00:19:15 +0300

    First commit =)

---
+++ Assets/Scripts/Menu/MenuItemStart.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItemStart : MonoBehaviour {
+    public Vector2 startPos, speedVec;
+	
+	private RectTransform rec;
+	private Vector2 needPosObj;
+
+	// Use this for initialization
+	void Start () {
+		rec = GetComponent<RectTransform>();
+		needPosObj = rec.anchoredPosition;
+		rec.anchoredPosition = startPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (speedVec.x > 0)
+		{
+			if (needPosObj.x > rec.anchoredPosition.x)
+				rec.anchoredPosition += new Vector2(speedVec.x, 0);
+		}
+		else if (speedVec.x < 0)
+		{
+			if (needPosObj.x < rec.anchoredPosition.x)
+				rec.anchoredPosition += new Vector2(speedVec.x, 0);
+		}
+		if (speedVec.y > 0)
+		{
+			if (needPosObj.y > rec.anchoredPosition.y)
+				rec.anchoredPosition += new Vector2(0, speedVec.y);
+		}
+		else if (speedVec.y < 0)
+		{
+			if (needPosObj.y < rec.anchoredPosition.y)
+				rec.anchoredPosition += new Vector2(0, speedVec.y);
+		}
+		
+		
+		/*if (speedVec.x > 0)
+        {
+			if (transform.position.x + speedVec.x > needPosObj.x)
+			{
+				rec.offsetMin = new Vector2(rec.offsetMin.x, 5f);
+			}
+			else
+			{ }
+        }
+        else
+        {
+            
+        }
+		if (speedVec.y > 0)
+        {
+			
+        }
+        else
+        {
+            
+        }
+        if (transform.position.x != needPosObj.y)
+        {
+            
+        }*/
+	}
+}
---
+++ Assets/Scripts/Menu/NameGameMainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NameGameMainMenu : MonoBehaviour {
+
+	public float delta1;
+	public float time1;
+	public float delta2;
+	public float time2;
+
+	private Outline ul;
+	private Color c_on;
+	private Color c_off;
+
+	IEnumerator blink1()
+	{
+		ul.effectColor = c_on;
+		yield return new WaitForSeconds(time1);
+		ul.effectColor = c_off;
+	}
+
+	IEnumerator blink2()
+	{
+		ul.effectColor = c_on;
+		yield return new WaitForSeconds(time2);
+		ul.effectColor = c_off;
+	}
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		ul = GetComponent<Outline>();
+
+		c_on = new Color(ul.effectColor.r, ul.effectColor.g, ul.effectColor.b, 1.0f);
+		c_off = new Color(ul.effectColor.r, ul.effectColor.g, ul.effectColor.b, 0.0f);
+		
+		InvokeRepeating("blink1", 1.0f, delta1);
+		InvokeRepeating("blink2", 1.0f, delta2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+}
